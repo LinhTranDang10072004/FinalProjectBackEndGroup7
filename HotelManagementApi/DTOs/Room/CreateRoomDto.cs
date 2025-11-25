617f386e3ccbc87f127c373abfd597c992b5d0aa
HieuBTFPT <buitrunghieucuber@gmail.com>
2025-11-25 10:39:27 +0700

    add CRUD(fix)

--- HotelManagementApi/DTOs/Room/CreateRoomDto.cs
+++ HotelManagementApi/DTOs/Room/CreateRoomDto.cs
@@ -4,9 +4,8 @@ namespace HotelManagementApi.DTOs.Room
 {
     public class CreateRoomDto
     {
-        public string RoomNumber { get; set; } = null!;
+        [Required, MaxLength(10)] public string RoomNumber { get; set; } = null!;
         public int Floor { get; set; }
         public int RoomTypeID { get; set; }
-        public bool IsActive { get; set; } = true;
     }
 }
--- HotelManagementApi/DTOs/Room/UpdateRoomDto.cs
+++ HotelManagementApi/DTOs/Room/UpdateRoomDto.cs
@@ -4,10 +4,8 @@ namespace HotelManagementApi.DTOs.Room
 {
     public class UpdateRoomDto
     {
-        public string? RoomNumber { get; set; }
+        [MaxLength(10)] public string? RoomNumber { get; set; }
         public int? Floor { get; set; }
-        public string? Status { get; set; }
-        public int? RoomTypeID { get; set; }
         public bool? IsActive { get; set; }
     }
 }
